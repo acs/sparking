77245fdcc404383d4ffb9b63142c47432047e6dd
Alvaro del Castillo <acs@bitergia.com>
2018-10-20 22:40:47 +0200

    First version of the WordCount Spark application using scala

---
+++ scala/WordCount/build.sbt
@@ -0,0 +1,8 @@
+name := "WordCount"
+
+version := "0.1"
+
+// scalaVersion := "2.12.7"
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
---
+++ scala/WordCount/src/main/scala/WordCount.scala
@@ -0,0 +1,21 @@
+// package main
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+object WordCount {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Word Count")
+    conf.setMaster("local")
+    val sc = new SparkContext(conf)
+    val readme = sc.textFile("/home/acs/devel/spark/README.md")
+    print(readme.first)
+    val words = readme.flatMap(line => line.split(" "))
+    val wordsCountPrep = words.map(word => (word, 1))
+    val wordsCount = wordsCountPrep.reduceByKey((acc, newval)=>acc+newval)
+    val wordsOrder = wordsCount.sortBy(kvPair => kvPair._2, false)
+    print(wordsOrder)
+    wordsOrder.saveAsTextFile("/tmp/wcIJ")
+  }
+}
