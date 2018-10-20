719176a3d67f26bee9469689825b85e9473b23d2
Alvaro del Castillo <acs@bitergia.com>
2018-10-20 23:05:57 +0200

    Improve doc and paths to files and folders in the WordCount sample

--- scala/WordCount/README.md
+++ scala/WordCount/README.md
@@ -4,7 +4,10 @@ In this sample, the words of a file are counted using Apache Spark.
 
 To run this sample:
 
+$ rm -rf /tmp/sparking-wordcount
 $ sbt compile
 $ sbt run
+
+You can check in `/tmp/sparking-wordcount` folder the words ordered by frequency of the README.md file.
 
 More details will come.
--- scala/WordCount/src/main/scala/WordCount.scala
+++ scala/WordCount/src/main/scala/WordCount.scala
@@ -1,5 +1,3 @@
-// package main
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
@@ -9,13 +7,13 @@ object WordCount {
     val conf = new SparkConf().setAppName("Word Count")
     conf.setMaster("local")
     val sc = new SparkContext(conf)
-    val readme = sc.textFile("/home/acs/devel/spark/README.md")
+    val readme = sc.textFile("README.md")
     print(readme.first)
     val words = readme.flatMap(line => line.split(" "))
     val wordsCountPrep = words.map(word => (word, 1))
     val wordsCount = wordsCountPrep.reduceByKey((acc, newval)=>acc+newval)
     val wordsOrder = wordsCount.sortBy(kvPair => kvPair._2, false)
     print(wordsOrder)
-    wordsOrder.saveAsTextFile("/tmp/wcIJ")
+    wordsOrder.saveAsTextFile("/tmp/sparking-wordcount")
   }
 }
